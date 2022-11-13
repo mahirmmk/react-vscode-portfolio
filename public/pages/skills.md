10ead5d18fef5e766d4b98beb89c36618b828a51
mahir <mahir9908@outlook.com>
2022-11-13 13:11:45 +0530

    initial commit

---
+++ public/pages/skills.md
@@ -0,0 +1,27 @@
+# 💻 Skills
+
+## General Tech Stacks
+- Web App Development
+- Web Development (Frontend & Backend)
+
+## Programming languages
+- JavaScript/TypeScript
+- React.js
+- Node-js
+- HTML
+- Css
+ - Bootstrap
+ - Tailwind
+
+## Databases
+- MSSQL
+- Mongo-DB
+
+## Cloud
+- Azure
+- GCP
+- Heroku
+- Render
+
+## Vcs
+- Git
---
+++ src/app/pages/links.tsx
@@ -0,0 +1,40 @@
+import {
+  FaMedium,
+  FaBlog,
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    index: 0,
+    title: "Find me on Github",
+    href: "https://github.com/mahirmmk",
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: "Find me on LinkedIn",
+    href: "https://www.linkedin.com/in/mohammad-mahir-082563159/",
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: "Contact me via email",
+    href: "mailto:mahir9908@outlook.com",
+    icon: <FaEnvelope />,
+  },
+  {
+    index: 3,
+    title: "Find me on Medium",
+    href: "https://medium.com/",
+    icon: <FaMedium />,
+  },
+  {
+    index: 4,
+    title: "Find me on Blog",
+    href: "https://mahirmmk.github.io/react-practice-project/",
+    icon: <FaBlog />,
+  },
+];
